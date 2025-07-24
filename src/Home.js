149c4bfcc465496c28bad01f645806e27b645088
Bryan Amds <bryan.amadeus1003@gmail.com>
2025-07-24 23:56:45 +0700

    Click Events

--- src/Home.js
+++ src/Home.js
@@ -1,7 +1,17 @@
 const Home = () => {
+
+    const handleClick = (e) => {
+        console.log('Hello, everynyan!', e);
+    }
+
+    const handleClickAgain = (name, e) => {
+        console.log('Hello '+ name + e.pageY);
+    }
     return ( 
         <div className="home">
             <h2>Homepage!</h2>
+            <button onClick={handleClick}>Click me!</button>
+            <button onClick={(e) => handleClickAgain('Mario', e)}>Click me again!</button>
         </div>
      );
 }
