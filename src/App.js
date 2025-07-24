3dac090c6850061ccaf880315da5d5c9235609b0
Bryan Amds <bryan.amadeus1003@gmail.com>
2025-07-24 21:37:01 +0700

    Dynamic Values in Templates

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const title = "Welcome to Meerkat Blog";
+  const likes = 10000;
+  // const person = { name: 'Titus', age: 300 };
+  const link = "https://www.youtube.com/watch?v=dQw4w9WgXcQ";
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="content">
+        <h1>{ title }</h1>
+        <p>Liked { likes } times</p>
+        {/* <p>{ person }</p> */}
+
+        <p>YAY</p>
+        <a href={ link }>Curious Link</a>
+
+      </div>
     </div>
   );
 }
