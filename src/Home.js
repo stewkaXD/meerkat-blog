fb6828120d699c1508d70c08dcc0168effd2f93c
Bryan Amds <bryan.amadeus1003@gmail.com>
2025-07-25 21:50:04 +0700

    Functions as Props

--- src/Home.js
+++ src/Home.js
@@ -5,15 +5,20 @@ const Home = () => {
 
     const [blogs, setBlogs] = useState([
         { title: 'First blog ever!', body: 'lorem ipsum idk', author: 'Mario', id: 1 },
-        { title: 'Web dev top tips', body: 'lorem ipsum idk too', author: 'Yoshi', id: 2 },
+        { title: 'Web dev top tips', body: 'lorem ipsum idk too', author: 'Mario', id: 2 },
         { title: 'School is so boring', body: 'lorem ipsum lmao', author: 'Luigi', id: 3 }
     ]);
 
-    return ( 
+    const handleDelete = (id) => {
+        const newBlogs = blogs.filter(blog => blog.id !== id);
+        setBlogs(newBlogs)
+    }
+
+    return (
         <div className="home">
-            <BlogList blogs={ blogs } title="All Blogs!"/>
+            <BlogList blogs={blogs} title="All Blogs!" handleDelete={handleDelete}/>
         </div>
-     );
+    );
 }
  
 export default Home;
