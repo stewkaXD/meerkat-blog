f8b504eba36e5c25fea5a1138f4f125c8ce4a2da
Bryan Amds <bryan.amadeus1003@gmail.com>
2025-07-31 00:13:59 +0700

    Making a Custom Hook

--- src/Home.js
+++ src/Home.js
@@ -1,33 +1,11 @@
-import { useState, useEffect } from "react";
 import BlogList from "./BlogList";
+import useFetch from "./useFetch";
 
 const Home = () => {
 
-    const [blogs, setBlogs] = useState(null);
-    const [isPending, setIsPending] = useState(true);
-    const [error, setError] = useState(null);
-
-    useEffect(() => {
-        setTimeout(() => {
-            fetch('http://localhost:8000/blogs')
-                .then(res => {
-                    if(!res.ok) { // throw error if server returned error
-                        throw Error('Could not fetch the data!')
-                    }
-                    return res.json();
-                })
-                .then(data => {
-                    setBlogs(data);
-                    setIsPending(false); // after the blogs are shown, take down loading screen
-                    setError(null);
-                })
-                .catch((err) => {
-                    setError(err.message);
-                    setIsPending(false); // make Loading... go away    
-                });
-        }, 1000);
-    }, []);
-
+    const {data: blogs, isPending, error} = useFetch('http://localhost:8000/blogs');
+    // grab the data, but call it blogs (in this context)
+    
     return (
         <div className="home">
             {error && <div>{ error }</div>}
---
+++ src/useFetch.js
@@ -0,0 +1,32 @@
+import { useState, useEffect } from "react";
+
+const useFetch = (url) => {
+    const [data, setData] = useState(null);
+    const [isPending, setIsPending] = useState(true);
+    const [error, setError] = useState(null);
+
+    useEffect(() => {
+        setTimeout(() => {
+            fetch(url)
+                .then(res => {
+                    if(!res.ok) { // throw error if server returned error
+                        throw Error('Could not fetch the data!')
+                    }
+                    return res.json();
+                })
+                .then(data => {
+                    setData(data);
+                    setIsPending(false); // after data are shown, take down loading screen
+                    setError(null);
+                })
+                .catch((err) => {
+                    setError(err.message);
+                    setIsPending(false); // make Loading... go away    
+                });
+        }, 500);
+    }, [url]);
+
+    return {data, isPending, error};
+}
+
+export default useFetch;
